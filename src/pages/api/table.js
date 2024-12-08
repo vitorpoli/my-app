a9bab748f3a230f885d22cc604c160660773425f
vitorpoli <vitor.poli@hotmail.com>
2024-12-08 13:34:57 +0000

    "Insert cors"

--- src/pages/api/table.js
+++ src/pages/api/table.js
@@ -1,6 +1,25 @@
 import mysql from 'mysql2/promise';
+import Cors from 'cors';
+
+const cors = Cors({
+  methods: ['GET'], // Permite apenas o método GET
+  origin: '*', // Permite qualquer origem
+});
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
 
 export default async function handler(req, res) {
+  await runMiddleware(req, res, cors); // Aplica o middleware CORS
+
   if (req.method === 'GET') {
     try {
       const connection = await mysql.createConnection({
@@ -22,6 +41,7 @@ export default async function handler(req, res) {
     res.status(405).json({ error: 'Método não permitido' });
   }
 }
+
 
 
 
