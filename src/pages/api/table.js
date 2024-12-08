556fb45b976092101c186507b29f83c9b012b38e
vitorpoli <vitor.poli@hotmail.com>
2024-12-08 12:10:18 +0000

    "Update connection mysql"

--- src/pages/api/table.js
+++ src/pages/api/table.js
@@ -1,17 +1,31 @@
-import db from '../../data/db';
+import mysql from 'mysql2/promise';
 
 export default async function handler(req, res) {
   if (req.method === 'GET') {
     try {
-      const [rows] = await db.query('SELECT Id, FirstName, LastName, Email FROM users');
-      res.status(200).json(rows); // Retorna os dados da tabela users
+      const connection = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+      });
+
+      // Consultando os dados dos usuários
+      const [rows] = await connection.query('SELECT Id, FirstName, LastName, Email FROM users');
+
+      // Fechando a conexão após a consulta
+      await connection.end();
+
+      // Respondendo com os dados
+      res.status(200).json(rows);
     } catch (error) {
-      console.error(error);
+      console.error('Erro ao buscar os usuários:', error);
       res.status(500).json({ error: 'Erro ao buscar usuários' });
     }
   } else {
-    res.status(405).json({ error: 'Método não permitido' }); // Para garantir que apenas GET seja permitido
+    res.status(405).json({ error: 'Método não permitido' });
   }
 }
+
 
 
